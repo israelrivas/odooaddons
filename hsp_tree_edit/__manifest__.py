19390a010ed23e3be7783c042dd32a5ea4e2f596
israelrivas <101383331+israelrivas@users.noreply.github.com>
2023-05-23 16:59:08 -0600

    0.1

---
+++ hsp_tree_edit/__manifest__.py
@@ -0,0 +1,33 @@
+# Copyright 2020  HSP
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "tree edit and mass editing by HSP",
+
+    'summary': """tree edit and mass edit 
+    tree视图可编辑和多记录编辑
+    """,
+   'license': 'LGPL-3',
+   'description': """
+    tree edit and mass edit 
+    tree视图可编辑和多记录编辑
+   """,
+    'author': 'HSP',
+    'website': "https://www.garage-kit.com",
+    'images': ['static/description/logo.png'],
+    'category': 'Tools',
+    'version': '13.0.1.0.0',
+  
+    'depends': [
+        'base', 'web',
+    ],
+    'data': [
+        #'views/template.xml'
+    ],
+    #'assets': [
+        #'hsp_tree_edit/static/src/js/list_controller.js',
+    #],
+    # 'demo': [
+    #     'demo/report.xml',
+    # ],
+    'installable': True,
+}
